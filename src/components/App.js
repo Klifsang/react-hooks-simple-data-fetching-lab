f6d852f30325dc73062989a6d313679377018dfd
Klif <sang.wicklife@student.moringaschool.com>
2024-02-03 10:46:18 +0300

    Lab completed

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,23 @@
+import { useEffect, useState } from "react";
+
 // create your App component here
+export default function App() {
+  const [imageUrl, setImageUrl] = useState(null);
+  useEffect(() => {
+    fetch("https://dog.ceo/api/breeds/image/random")
+      .then((res) => res.json())
+      .then((data) => {
+        setImageUrl(data.message);
+      });
+  });
+
+  return (
+    <div>
+      {!imageUrl ? (
+        <p>Loading...</p>
+      ) : (
+        <img src={imageUrl} alt="A Random Dog" />
+      )}
+    </div>
+  );
+}
